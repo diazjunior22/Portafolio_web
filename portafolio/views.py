ebb4cf87e6b7a360735c210821f332f1d15d288c
170588305+eldiaz22@users.noreply.github.com. <diazjunior354@gmail.com>
2025-09-09 14:37:33 -0500

    aaa

--- portafolio/views.py
+++ portafolio/views.py
@@ -11,26 +11,57 @@ from django.core.mail import EmailMessage
 from blog.models import Post
 
 
-
+#784303315bfb2d606d4296bc75dd96c1
 
 from django.contrib import messages
 
+import  requests
 
 # Create your views here.
 
 
-def enviar_correo(nombre, email, asunto, mensaje):
-            EmailMessage(
-                subject = f'CONTACTO EMAILL {nombre}' ,
-                body = f'{mensaje} + {asunto}' ,
-                from_email='juniordiazpalacio9@gmail.com',
-                to = ['juniordiazpalacio9@gmail.com'],
-                cc = [],
-                reply_to=[email]
+# def enviar_correo(nombre, email, asunto, mensaje):
+#             EmailMessage(
+#                 subject = f'CONTACTO EMAILL {nombre}' ,
+#                 body = f'{mensaje} + {asunto}' ,
+#                 from_email='juniordiazpalacio9@gmail.com',
+#                 to = ['juniordiazpalacio9@gmail.com'],
+#                 cc = [],
+#                 reply_to=[email]
                 
                 
-            ).send()
+#             ).send()
 
+
+import mailtrap as mt
+
+def enviar_correo(nombre, email, asunto, mensaje):
+    token = "d645d0fae3fca7ab6351e54d83591802"
+    # payload = {
+    #     "from": {"email": "hello@demomailtrap.com", "name": nombre},
+    #     "to": [{"email": "juniordiazpalacio9@gmail.com"}],
+    #     "reply_to": [{"email": email}],
+    #     "subject": f"CONTACTO EMAIL {nombre} - {asunto}",
+    #     "text": mensaje,
+    #     "category": "Formulario Web"
+    # }# Pon aquí tu API Token de Mailtrap (Email Sending > API Tokens)
+
+    # headers = {
+    #         "Authorization": f"Bearer {token}",
+    #         "Content-Type": "application/json"
+    # }
+    # response = requests.post( url, headers=headers, json=payload)
+    mail = mt.Mail(
+        sender=mt.Address(email="juniordiazpalacio9@gmail.com", name=nombre),  
+        to=[mt.Address(email="juniordiazpalacio9@gmail.com")],          
+        subject=f"CONTACTO EMAIL {nombre} - {asunto} Responder a: {email}",
+        text=f"De: {nombre} <{email}>\n\nMensaje:\n{mensaje}",
+        category="Formulario Web",
+    )
+
+    client = mt.MailtrapClient(token=token)  
+    response = client.send(mail)
+    return response 
 
 
 def home(request):
