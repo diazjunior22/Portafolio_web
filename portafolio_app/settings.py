71f820951c7f3ba1a18fd0cf9b4a9b9fbf66e564
unknown <diazjunior354@gmail.com>
2026-03-21 15:43:47 -0500

    cambios

--- portafolio_app/settings.py
+++ portafolio_app/settings.py
@@ -116,22 +116,25 @@ WSGI_APPLICATION = 'portafolio_app.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 
-if DEBUG in ['True' , True]:
+USE_CLOUD_DB = os.getenv('USE_CLOUD_DB') == 'True'
+
+if USE_CLOUD_DB:
+    # Usar PostgreSQL de la nube (Railway)
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.getenv('DATABASE_URL'),
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    # Usar SQLite local
     DATABASES = {
       'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
- }
-}
-else:
-    DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
-
+      }
+    }
     # DATABASES = {
     #         'default': {   
     #         'ENGINE': 'django.db.backends.postgresql',
